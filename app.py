efaa4753cf9a4c6ae79659beb375554130232669
Jeff <00253112@email.ntou.edu.tw>
2021-06-15 04:39:07 +0800

    use '{% extends %}' to build a base

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = {'username' : 'root'}
-    rows = [
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
-        {'name': 'Python', 'age': 27},
+    postInput = [
+        {
+            "author": {"username": "Pan"},
+            "body": "You can do it :)"
+        },
+        {
+            "author": {"username": "test"},
+            "body": "this is a test textAAA"
+        }
     ]
-    return render_template('index.html', name=name, rows=rows)
+    return render_template('index.html', name=name, postOutput=postInput)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
