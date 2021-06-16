69adf6098d8a148811bd61f069d7966010bb8d1b
Jeff <00253112@email.ntou.edu.tw>
2021-06-16 19:57:51 +0800

    0.2

--- twittor/route.py
+++ twittor/route.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from twittor.forms import LoginForm
 
 def index():
@@ -21,5 +21,12 @@ def index():
 
 def login():
     form = LoginForm(meta={'csrf': False})
-
+    if form.validate_on_submit():
+        msg = "username={}, password={}, remember_me={}".format(
+            form.username.data,
+            form.password.data,
+            form.remember_me.data
+        )
+        print(msg)
+        return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
