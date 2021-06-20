98b06c71645758f7b31d84e589522f6d324ecf93
Jeff <00253112@email.ntou.edu.tw>
2021-06-21 00:00:19 +0800

    deleted one more weird line from __init__.py

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -10,7 +10,6 @@ def create_app():
     app = Flask(__name__)
     db.init_app(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:twittor.db:"
-    db.init_app(app)
     migrate.init_app(app, db)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
