5875224af6a07a523a8ada3f2c7f50311fae4722
Jeff <00253112@email.ntou.edu.tw>
2021-06-21 00:34:30 +0800

    a break

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from twittor.route import index, login
 
 db = SQLAlchemy()
 migrate = Migrate()
 
+from twittor.route import index, login
+
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///twittor.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:twittor.db:"
     migrate.init_app(app, db)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
