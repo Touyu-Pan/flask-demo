0e8df385103a910bd5636031d091f882bb66187d
Jeff Pan <jeffpan84081441@gmail.com>
2022-03-16 14:04:55 +0800

    remove manager.py and edit related file

--- manager.py
+++
@@ -1,8 +0,0 @@
-from flask_script import Manager
-from twittor import create_app
-
-app = create_app()
-manager = Manager(app)
-
-if __name__ == "__main__":
-    manager.run()
--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+export FLASK_APP=twittor
 
-python manager.py db migrate
-python manager.py db upgrade
+flask db migrate
+flask db upgrade
 gunicorn --bind=0.0.0.0:8000 --log-level info --workers 4 twittor.wsgi:application
